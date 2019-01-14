7b58b13c7d7b7fe7ca76321a7cb5d700dda3f694
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-14 14:53:34 +0100

    Always include timing.

--- include/timing.h
+++ include/timing.h
@@ -1,36 +1,18 @@
 
-#ifndef TIMING_H
-#define TIMING_H
-
-//#define TIME
-
-//#ifdef TIME
-#include <chrono>
 #include <map>
+#include <chrono>
+
+#pragma once
 
 struct Time {
     double value = 0;
 };
 
 
-#ifdef TIME
 #define MARK_TIME(name) name = std::chrono::high_resolution_clock::now();
 
 #define ADD_TIME_SINCE(timer_name, mark) \
 	queue.finish(); \
     time_map[#timer_name].value += \
 	std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-mark).count();
-
-#endif
-
-#ifndef TIME
-std::cout << "No rfi kernel timing" << std::endl;
-
-#define MARK_TIME(name) 
-
-#define ADD_TIME_SINCE(timer_name, mark) 
-
-#endif
-
-#endif
 
